12bd2cc84962466bd853f0dd122a1987ac35622d
Jain-Daksh <daksh44jain@gmail.com>
2022-08-19 10:52:48 +0530

    carousel code added

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import '../styles/globals.css'
-import Script from "next/script"
 import '../design/common.css'
 import 'bootstrap/dist/css/bootstrap.css';
+import {useEffect} from 'react'
 import '../design/navbar.css'
 import '../design/about.css'
 import '../design/whyus.css'
@@ -11,11 +11,9 @@ import '../design/footer.css'
 import '../design/images.css'
 import '../design/homepage.css'
 function MyApp({ Component, pageProps }) {
-  <Script
-src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
-integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW"
-crossorigin="anonymous"/>
+  useEffect(() => {
+    require('bootstrap/dist/js/bootstrap.bundle.min.js');
+  }, []);
   return <Component {...pageProps} />
 }
-
 export default MyApp
